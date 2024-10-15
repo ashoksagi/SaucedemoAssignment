4f196a0717d983a581b6a63b31a7938777dc48c7
ashoksagi <ashok.sagi@gmail.com>
2024-10-15 13:54:15 -0400

    Update JSONComparator.java

--- cucumberBDDSample/src/main/java/cucumber/testbase/JSONComparator.java
+++ cucumberBDDSample/src/main/java/cucumber/testbase/JSONComparator.java
@@ -18,7 +18,7 @@ import org.json.JSONObject;
 public class JSONComparator {
 
     private JSONObject configJSON = null;
-    private boolean isSame = true;
+    public boolean isSame = true;
 
     private Stack jsonPath = new Stack();
 
@@ -29,17 +29,31 @@ public class JSONComparator {
     public String getCurrentJSONPath(String currenyKey) {
         String currentJSONPath = String.join(".", jsonPath);
         if (currenyKey != null) {
-            System.out.println("currentJSONPath:" + currentJSONPath + "." + currenyKey);
+            //System.out.println("currentJSONPath:" + currentJSONPath + "." + currenyKey);
             return currentJSONPath + "." + currenyKey;
         } else {
-            System.out.println("currentJSONPath:" + currentJSONPath);
+            //System.out.println("currentJSONPath:" + currentJSONPath);
             return currentJSONPath;
         }
     }
+    
+    private void pushToJSONPath(String key){
+       if(key!=null){ 
+         jsonPath.push(key);
+       }
+       //System.out.println("Pushed:"+key);
+    }
+    
+    private void popFromJSONPath(){
+       String key=String.valueOf(jsonPath.pop());
+       //System.out.println("Popped:"+key);
+    }
 
     // Recursive method to compare two JSONObjects
-    public boolean compareJSONObjects(JSONObject json1, JSONObject json2, String parentKey) {
-        jsonPath.push(parentKey);
+    public boolean compareJSONObjects(JSONObject json1, JSONObject json2, String parentKey) throws Exception {
+        if(parentKey!=null){
+          pushToJSONPath(parentKey);
+        }
         isSame = json1 == json2;
         Set<String> set1 = json1.keySet();
         Set<String> set2 = json2.keySet();
@@ -48,11 +62,12 @@ public class JSONComparator {
 
         Set<String> disjointInSet2 = new HashSet<>(set2);
         disjointInSet2.removeAll(set1);
+        String currentJSONPath=getCurrentJSONPath(null);
         if (disjointInSet1.size() > 0) {
-            System.out.println("Elements/Props Extra IN JSON1: " + disjointInSet1);
+            System.out.println("Elements/Props Extra IN JSON1: " + disjointInSet1+" AT PATH"+currentJSONPath);
         }
         if (disjointInSet2.size() > 0) {
-            System.out.println("Elements/Props Extra IN JSON2" + disjointInSet2);
+            System.out.println("Elements/Props Extra IN JSON2" + disjointInSet2+" AT PATH"+currentJSONPath);
         }
 
         Set<String> commonKeys = new HashSet<>(set1);
@@ -62,6 +77,8 @@ public class JSONComparator {
             Object value1 = json1.get(key);
             Object value2 = json2.get(key);
 
+            currentJSONPath=getCurrentJSONPath(key);
+            
             if (value1 instanceof JSONObject && value2 instanceof JSONObject) {
                 if (!compareJSONObjects((JSONObject) value1, (JSONObject) value2, key)) {
                     isSame = false;
@@ -71,29 +88,35 @@ public class JSONComparator {
                     isSame = false;
                 }
             } else if (!value1.equals(value2)) {
-                System.out.println("Primitive value in both JSON not equal for key: " + key);
+                System.out.println("Primitive value not matching: " + currentJSONPath +" : "+ String.valueOf(value1) + "!=" + String.valueOf(value2));
                 isSame = false;
             }
         }
-        jsonPath.pop();
+        if(parentKey!=null){
+          popFromJSONPath();
+        }
         return false;
     }
 
     // Recursive method to compare two JSONArrays
-    public boolean compareJSONArrays(JSONArray array1, JSONArray array2, String parentKey) {
+    public boolean compareJSONArrays(JSONArray array1, JSONArray array2, String parentKey) throws Exception {
         if (array1.length() == 0 && array2.length() == 0) {
             return true;
         }
-        jsonPath.push(parentKey+"[]");
+        pushToJSONPath(parentKey+"[]");
         if (array1.length() != array2.length()) {
             System.out.println("Lengths of arrays are not matching");
         }
         ExtendedJSONArray eArray1 = new ExtendedJSONArray(array1);
         ExtendedJSONArray eArray2 = new ExtendedJSONArray(array2);
 
-        getCurrentJSONPath(null);
+        String currentJSONPath=getCurrentJSONPath(null);
 
-        String idGenJsonExpr = configJSON.optString(parentKey);
+        String idGenJsonExpr = configJSON.optString(currentJSONPath);
+        if(idGenJsonExpr==""){
+           throw new Exception("KEY_MISSING_CONFIG_JSON:"+currentJSONPath);
+        }
+        System.out.println("ID gen expression for each element in the array: " + idGenJsonExpr);
         Map<Object, JSONObject> array1Map = eArray1.getIndexedJSONObjectMembers(idGenJsonExpr, 1, parentKey);
         Map<Object, JSONObject> array2Map = eArray2.getIndexedJSONObjectMembers(idGenJsonExpr, 2, parentKey);
         eArray1.comparePrimitives(eArray2);
@@ -102,7 +125,14 @@ public class JSONComparator {
         commonElementIds.retainAll(array2Map.keySet());
 
         for (Object eleID : commonElementIds) {
-            compareJSONObjects(array1Map.get(eleID), array2Map.get(eleID), String.valueOf(eleID));
+            String rootJsonElement=configJSON.optString("ROOT_ARRAY_ELEMENT");
+            if(currentJSONPath.equalsIgnoreCase(rootJsonElement)){
+                System.out.println("**************Start**************"+parentKey+":"+String.valueOf(eleID));
+            }
+            compareJSONObjects(array1Map.get(eleID), array2Map.get(eleID),null);
+            if(currentJSONPath.equalsIgnoreCase(rootJsonElement)){
+                System.out.println("**************End**************"+parentKey+":"+String.valueOf(eleID)+"\n");
+            }
         }
 
         eArray1.compareJSONArrays(eArray2);
@@ -113,12 +143,12 @@ public class JSONComparator {
         Set<Object> disjointInSet2 = new HashSet<>(array2Map.keySet());
         disjointInSet2.removeAll(array1Map.keySet());
         if (disjointInSet1.size() > 0) {
-            System.out.println("Generated Id property: " + idGenJsonExpr + " JSON1 extra:" + disjointInSet1);
+            System.out.println("JSON1 extra:" + disjointInSet1+" AT PATH:"+currentJSONPath);
         }
         if (disjointInSet2.size() > 0) {
-            System.out.println("Generated Id property: " + idGenJsonExpr + " JSON2 extra:" + disjointInSet2);
+            System.out.println("JSON2 extra:" + disjointInSet2+" AT PATH:"+currentJSONPath);
         }
-        jsonPath.pop();
+        popFromJSONPath();
         return false;
     }
 }
